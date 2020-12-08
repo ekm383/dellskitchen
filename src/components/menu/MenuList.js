6e1e1f6a26583a8568a50d1b3fef3ae7c7386cb7
Eric Manongdo <superscript@Erics-MBP.home>
2020-12-08 09:09:14 -1000

    Prices Added

--- src/components/menu/MenuList.js
+++ src/components/menu/MenuList.js
@@ -16,25 +16,21 @@ const MenuList = () => {
       <div id="menu" className="menu-container">
         <div className="column column1">
           <MenuItem
-            dish="Hayashi Beef Rice"
+            dish="Hayashi Beef Rice $12"
             description="Beef, Onions, and Mushroom in Demi Sauce"
           />
           <MenuItem
-            dish="Maui Style Short Rib"
+            dish="Maui Style Short Rib $16"
             description="Short Rib in our House Soy/Garlic Marinade and Grilled"
-          />
-          <MenuItem
-            dish="Soy Ginger Beef"
-            description="Our Version of Teriyaki Beef Sauteed with Vegetables"
           />
         </div>
         <div className="column column2">
           <MenuItem
-            dish="Gyu Don"
-            description="Chef Masa's Own Special Recipe. Slow Cook Thin Slices of Beef Braised in our Special Sauce and Served with Onsen Tamago"
+            dish="Gyu Don $10"
+            description="LIMITED 10 ORDERS PER DAY. Chef Masa's Own Special Recipe. Slow Cook Thin Slices of Beef Braised in our Special Sauce and Served with Onsen Tamago"
           />
           <MenuItem
-            dish="Loco Moco"
+            dish="Loco Moco $12"
             description="Homemade Hamburger Steak, Two Eggs, and Kabayaki Gravy"
           />
         </div>
@@ -51,27 +47,24 @@ const MenuList = () => {
       <div className="menu-container">
         <div className="column column1">
           <MenuItem
-            dish="Tonkatsu"
+            dish="Tonkatsu $11"
             description="Boneless Rib Chop Battered in Panko. Served with DK Katsu Sauce"
           />
           <MenuItem
-            dish="Grilled Pork Chop"
+            dish="Grilled Pork Chop $11"
             description="Marinated Pork Chop. Grilled and Served with Pineapple Mustard Sauce"
           />
           <MenuItem
-            dish="Kim Chee Pork"
+            dish="Kim Chee Pork $9.50"
             description="Por Sauteed with Homemade Kim Chee and Vegetables"
           />
         </div>
         <div className="column column2">
           <MenuItem
-            dish="Kawagoe Gyoza"
+            dish="Kawagoe Gyoza $9.50"
             description="Our Famous Homemade Gyoza. Made Fresh Daily."
           />
-          <MenuItem
-            dish="Yakisoba"
-            description="Mio Pasta Spaghetti Noodles Sauteed with Pork and Vegetables"
-          />
+          <MenuItem dish="Ginger Pork $9.50" description="" />
         </div>
       </div>
       {/* Chicken Section */}
@@ -86,25 +79,25 @@ const MenuList = () => {
       <div className="menu-container">
         <div className="column column1">
           <MenuItem
-            dish="Chicken Katsu Curry"
+            dish="Chicken Katsu Curry $11"
             description="Chicken Katsu Served with Special Curry Sauce"
           />
           <MenuItem
-            dish="Nori/Shiso Chicken"
+            dish="Nori/Shiso Chicken $10.50"
             description="Marinated Chicken Thigh, Mochiko Flour, Nori and Shiso"
           />
           <MenuItem
-            dish="Soy Ginger Chicken"
+            dish="Soy Ginger Chicken $9.50"
             description="Our Version of Teriyaki Chicken"
           />
         </div>
         <div className="column column2">
           <MenuItem
-            dish="Kawagoe Karaage"
+            dish="Kawagoe Karaage $9.50"
             description="Izakawa Kawagoe Famous Chicken Karaage"
           />
           <MenuItem
-            dish="Garlic Chili Chicken"
+            dish="Garlic Chili Chicken $10"
             description="Our Karaage Chicken Dressed in a Chili/Garlic Dressing"
           />
         </div>
@@ -121,23 +114,24 @@ const MenuList = () => {
       <div className="menu-container">
         <div className="column column1">
           <MenuItem
-            dish="Ahi Katsu"
+            dish="Ahi Katsu $15.00"
             description="Ahi Loin Battered in Panko and Shallow Fried. Served with our Homemade pickled aioli"
           />
           <MenuItem
-            dish="Salmon Furikake Katsu"
+            dish="Salmon Furikake Katsu $12"
             description="Salmon Battered in Panko and 'AO' Nori and Shallow Fried. Served with our Homemade Pickled Aioli"
           />
         </div>
         <div className="column column2">
           <MenuItem
-            dish="Miso Salmon"
+            dish="Miso Salmon $12"
             description="Pacific Salmon Marinated in Miso and Grilled"
           />
           <MenuItem
-            dish="Seafood Croquette"
+            dish="Seafood Croquette $12"
             description="Bay Scallop, Shrimp and Imitation Crab. Chef Masa's Honey Shallot Sauce"
           />
+          <MenuItem dish="Yakisoba $9.50" description="" />
         </div>
       </div>
       {/* Vegetarian Section */}
@@ -152,17 +146,17 @@ const MenuList = () => {
       <div className="menu-container">
         <div className="column column1">
           <MenuItem
-            dish="Cauliflower Kalbi"
+            dish="Cauliflower Kalbi $11"
             description="Homemade Soy/Garlic Marinated and Grilled"
           />
           <MenuItem
-            dish="Miso Eggplant"
+            dish="Miso Eggplant $11"
             description="Broiled Eggplant Glazed with Miso and Furikake/Parmesan Breadcrumbs"
           />
         </div>
         <div className="column column2">
           <MenuItem
-            dish="Beyond Meat 'Hamburger Steak'"
+            dish="Beyond Meat 'Hamburger Steak' $11"
             description="Grilled Plant Based Patty Accompanied with 'Masa Kaitan Sauce'"
           />
         </div>
